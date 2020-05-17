53a6c47e80e50fe01eed627fd3bf1cdb59dfda15
JeremySilvaSilva <jeremysilvasilva@gmail.com>
2020-05-17 11:40:09 -0400

    ADD : axios client

---
+++ src/services/index.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+const urlBase = 'https://swapi.co/api'
+
+const readUrl = (url = '') =>
+  url.startsWith('http://') || url.startsWith('https://') ? url : `${urlBase}/${url}`
+
+const get = (url = '', headers = {}) => axios.get(readUrl(url), {
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    ...headers
+  }
+})
+
+const post = (url = '', body = {}, headers = {}) => axios.post(readUrl(url), body, {
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    ...headers
+  }
+})
+
+const put = (url = '', body = {}, headers = {}) => axios.put(readUrl(url), body, {
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    ...headers
+  }
+})
+
+const del = (url = '', headers = {}) => axios.delete(readUrl(url), {
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    ...headers
+  }
+})
+
+export default {
+  get,
+  post,
+  put,
+  delete: del
+}
